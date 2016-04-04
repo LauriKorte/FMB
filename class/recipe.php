52a2ee4c509d896bb9971b2ccafb2e78fb040a88
LauriKorte <lauri.korte@hotmail.com>
2016-04-04 10:34:17 +0300

    Update da classes

--- class/recipe.php
+++ class/recipe.php
@@ -1,8 +1,112 @@
 <?php
 
+	class AmountOfAttention
+	{
+		public $id;
+		public $name;
+		public $description;
+	}
+	
+	class Difficulty
+	{
+		public $id;
+		public $name;
+		public $description;
+	}
+	
+	class DishType
+	{
+		public $id;
+		public $name;
+		public $description;
+	}
+	
+	class Ingredient
+	{
+		public $id;
+		public $name;
+		public $ingredientUnit;
+		public $ingredientType;
+		public $ingredientStorage;
+	}
+	
+	class IngredientInStore
+	{
+		public $id;
+		public $ingredient;
+		public $amount;
+	}
+	
+	class IngredientStorage
+	{
+		public $id;
+		public $name;
+		public $description;
+	}
+	
+	class IngredientType
+	{
+		public $id;
+		public $name;
+		public $description;
+	}
+	
+	class IngredientUnit
+	{
+		public $id;
+		public $name;
+		public $shorthand;
+	}
+	
+	class ManufacturingTime
+	{
+		public $id;
+		public $name;
+		public $minimumTime;
+		public $maximumTime;
+	}
+	
+	class Rating
+	{
+		public $id;
+		public $description;
+		public $stars;
+	}
+	
 	class Recipe
 	{
 		public $id;
 		public $name;
-		public $ingredients;
+		public $description;
+			public $manufacturingTime;
+			public $resultType;
+			public $amountOfAttention;
+			public $difficulty;
+			public $dishType;
 	}
+	
+	class RecipeHasIngredient
+	{
+		public $id;
+		public $amount;
+		public $recipe;
+		public $ingredient;
+	}
+	
+	class RecipeHistory
+	{
+		public $id;
+		public $recipe;
+		public $date;
+		public $rating;
+		public $personaleComment;
+	}
+	
+	class ResultType
+	{
+		public $id;
+		public $name;
+		public $description;
+	}
+	
+	
