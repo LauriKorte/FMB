d2af7fb8019d2439ec57a689c63ab88a66dadb66
Miro Nousiainen <mirono@suomi24.fi>
2016-04-12 10:56:53 +0300

    Update navbar.php

--- class/content/navbar.php
+++ class/content/navbar.php
@@ -5,26 +5,37 @@ class NavBarStyle extends Style
 {
 	public function displayHeader()
 	{
-		echo ("<hr />");
+	
 	}
 	public function displayItem($arguments)
 	{
-		echo ("<div>");
+		echo ('<nav class="navbar navbar-default" role="navigation">
+				<div class="navbar-header">
+					 
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button> <a class="navbar-brand" href="#">OGR</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">');
 
 		if (!is_null($arguments['links']))
 		{
 			foreach ($arguments['links'] as $text => $target)
 			{
-				echo ("<a href='{$target}'/>{$text}</a><br>");
+				echo ("<li><a href='{$target}'/>{$text}</a></li>");
 			}
 		}
 	}
 	public function displayItemEnd()
 	{
-		echo ("</div>");
+		echo ('</ul>
+		       </div>
+		       </nav>');
 	}
 	public function displayFooter()
 	{
-		echo ("<hr />");
+	
 	}
 }
