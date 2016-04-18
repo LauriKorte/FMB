9b786fe096f7fb88188b75d10755189cbab10724
LauriKorte <lauri.korte@hotmail.com>
2016-04-18 09:57:10 +0300

    Update abootStyle.php

--- class/content/abootStyle.php
+++ class/content/abootStyle.php
@@ -9,7 +9,7 @@ class AbootStyle extends Style
 			<h1>About our Team</h1>
 			We are a bunch of students trying to make their way on this Earth and to fkjdsfkjfh<p>
 			We made this RecipeBook for we wanted some place to store our precious recipes of our fine cookies and other stuff. This program was also designed to be used as course exercise for the Palvelinohjelmointi IIM50300. It also serves the same purpose for Ohjelmistosuunnittelu IIO11100 course. Program uses our own database we made for Tietokannat TTZC0800 course.<p>
-			<b>Creators of this program:</b> Lauri 'Master of Destruction' Korte, Antti 'Master of Disaster' Mäkelä &amp Miro.
+			<b>Creators of this program:</b> Lauri 'Master of Destruction' Korte, Antti 'Master of Disaster' Mäkelä &amp; Miro.
 		</div>
 		<?php
 	}
