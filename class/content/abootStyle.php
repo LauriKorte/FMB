b79a3998186f7a1d0f077f2b45172f52d53afaac
LauriKorte <lauri.korte@hotmail.com>
2016-04-18 09:12:45 +0300

    add abootStyle

---
+++ class/content/abootStyle.php
@@ -0,0 +1,13 @@
+<?php
+require_once(__DIR__ . "/../site/style.php");
+class AbootStyle extends Style
+{
+	public function displayItem($arguments)
+	{
+		?>
+		<h1>About our Team</h1>
+		We are a bunch of students trying to make their way on this Earth. We are studying at JAMK University of Applied Sciences.<p>
+		We made this RecipeBook for we wanted some place to store our precious recipes of our fine cookies and other stuff. blaa blaa...
+		<?php
+	}
+}
